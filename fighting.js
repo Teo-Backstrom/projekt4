49154a0fbe19fee7e17edc368a2096bbb4770675
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-05-04 15:03:55 +0200

    Hp system för fighting game med knappar småProblem

--- fighting.js
+++ fighting.js
@@ -1,5 +1,7 @@
 let playerHp = 100;
 let pcHp = 100;
+let damage = 0;
+let result = 0;
 
 function choosePlayer() {
     document.querySelector('.espeon').addEventListener("click", function() {
@@ -23,4 +25,75 @@ function choosePlayer() {
     
     }
 
+    function attackChoise() {
+        return Math.floor(Math.random() * 2) ;
+    }
+
+    function roll() {
+        let sum = Math.floor(Math.random() * 6) + 1;
+        return sum * 10;
+    }
+
+    function playerAttackHealthDisplay(number) {
+        let placeholder = document.querySelector('.enemy-health');
+        pcHp = pcHp - number;
+        placeholder.innerHTML = pcHp;
+      }
+
+      function enemyHealHealthDisplay(number) {
+      let placeholder = document.querySelector('.enemy-health');
+      pcHp = pcHp + number;
+      placeholder.innerHTML = pcHp;
+      }
+
+      function enemyAttackHealthDisplay(number) {
+        let placeholder = document.querySelector('.player-health');
+        playerHp = playerHp - number;
+        placeholder.innerHTML = playerHp;
+      }
+      function playerHealHealthDisplay(number) {
+        let placeholder = document.querySelector('.player-health');
+        playerHp = playerHp + number;
+        placeholder.innerHTML = playerHp;
+      }
+      
+      document.querySelector('.attack').onclick = function() {
+        damage = roll();
+            playerAttackHealthDisplay(damage);
+            result = attackChoise();
+           console.log(result); 
+        if (result === 0) {
+            damage = roll();
+            enemyAttackHealthDisplay(damage);
+        } else{
+            damage = roll();
+            enemyHealHealthDisplay(damage);
+        }
+        
+      }
+
+      document.querySelector('.heal').onclick = function() {
+          console.log("1");
+        damage = roll();
+        console.log("2");
+        playerHealHealthDisplay(damage);
+        console.log("3");
+
+        result = attackChoise();
+        if (result === 0) {
+            damage = roll();
+            enemyAttackHealthDisplay(damage);
+        } else{
+            damage = roll();
+            enemyHealHealthDisplay(damage);
+        }
+        
+      }
+
+
     choosePlayer();
+
+    function playerAttackHealthDisplay(number) {
+        pcHp = document.querySelector('.enemy-health');
+        pcHp.innerHTML = pcHp - number;
+      }
